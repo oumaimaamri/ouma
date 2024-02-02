e439a729fcc9f71b803250f5e71c5ef05e4c6b01
prof <prof@isikef.cri>
2024-02-02 13:59:20 +0100

    message

---
+++ page.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ma page</title>
+</head>
+<body>
+ <?php 
+ $nom="ouma";
+ $age=24;
+ echo "<h1> bienvenue sur ma page , $nom! </h1>;"  
+ echo "<p> vous avez   $age ans,  </p>;"  
+</body>
+</html>
